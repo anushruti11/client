ac4501b3e4a16a842da79c730779f76f88095f0b
Abhishek Kumar <abhishek022kk@gmail.com>
2021-12-01 00:47:53 +0530

    add some changes

--- src/components/Homepage/TopDestinations.jsx
+++ src/components/Homepage/TopDestinations.jsx
@@ -1,19 +1,21 @@
 import './Homepage.css';
 
-const TopDestination = () => {
+const TopDestination = ({ item }) => {
+  console.log(item.places);
   return (
-    <>
-      <div style={{ padding: '1rem' }}>
-        <div
-          style={{
-            marginBottom: '2rem',
-            textAlign: 'center',
-            fontSize: '2rem',
-            fontWeight: '700',
-          }}
-        >
-          Explore our top destinations
-        </div>
+    <div
+      style={{
+        borderRadius: '50px',
+        boxShadow: '30px 30px 60px #080c14,-30px -30px 60px #162034',
+        maxWidth: '1200px',
+        margin: '2rem',
+      }}
+    >
+      <div
+        style={{
+          padding: '1rem',
+        }}
+      >
         <div
           style={{
             textAlign: 'center',
@@ -23,10 +25,7 @@ const TopDestination = () => {
         >
           <div style={{ width: '60rem', display: 'flex' }}>
             <div>
-              <img
-                src="https://cdn.getyourguide.com/img/location/5ffeb528d3d6f.jpeg/68.webp"
-                alt="Bihar"
-              />
+              <img src={item.mainImg} alt="Bihar" width="300" height="300" />
             </div>
             <div style={{ padding: '0rem 1rem 1rem 1rem' }}>
               <div
@@ -36,14 +35,9 @@ const TopDestination = () => {
                   fontWeight: '500',
                 }}
               >
-                Bihar
+                {item.name}
               </div>
-              <div>
-                In ancient and classical India, the area that is now Bihar was
-                considered a centre of power, learning, and culture.[15] From
-                Magadha arose India's first empire, the Maurya empire, as well
-                as one of the world's most widely adhered-to religions:
-              </div>
+              <div>{item.description}</div>
               {/* badges */}
               <div
                 style={{
@@ -53,17 +47,113 @@ const TopDestination = () => {
                   justifyContent: 'space-evenly',
                 }}
               >
-                <div className="destination-badge">Sightseeing Tours</div>
-                <div className="destination-badge">Sightseeing Tours</div>
-                <div className="destination-badge">Sightseeing Tours</div>
-                <div className="destination-badge">Sightseeing Tours</div>
-                <div className="destination-badge">Sightseeing Tours</div>
+                {item.badges.map((badge) => {
+                  return <div className="destination-badge">{badge}</div>;
+                })}
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+      {/* destinations */}
+      <div style={{ padding: '1rem' }}>
+        <div
+          style={{
+            marginBottom: '2rem',
+            textAlign: 'center',
+            fontSize: '1.5rem',
+            fontWeight: '700',
+          }}
+        >
+          Iconic Places
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+          }}
+        >
+          {item.places.map((place) => {
+            return (
+              <div
+                style={{
+                  background: '#fff',
+                  width: '300px',
+                  height: '200px',
+                  padding: '0.5rem',
+                  margin: '1rem',
+                }}
+              >
+                <img src={place.subImg} alt="item" width="200" height="80" />
+                <div
+                  style={{
+                    color: '#63687A',
+                    fontSize: '1rem',
+                    textAlign: 'center',
+                  }}
+                >
+                  {place.area}
+                </div>
+                <div style={{ color: '#63687A', marginBottom: '0.5rem' }}>
+                  {place.description}
+                </div>
+                <input type="button" value="Order Now!" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      {/* antique things */}
+      <div style={{ padding: '1rem' }}>
+        <div
+          style={{
+            marginBottom: '2rem',
+            textAlign: 'center',
+            fontSize: '1.5rem',
+            fontWeight: '700',
+          }}
+        >
+          Top Antique Things Indegineous To That Place
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+          }}
+        >
+          {item.items.map((thing) => {
+            return (
+              <div
+                style={{
+                  background: '#fff',
+                  width: '300px',
+                  height: '200px',
+                  padding: '0.5rem',
+                  margin: '1rem',
+                }}
+              >
+                <img src={thing.itemImg} alt="item" width="200" height="80" />
+                <div
+                  style={{
+                    color: '#000',
+                    fontSize: '1rem',
+                    textAlign: 'center',
+                  }}
+                >
+                  {thing.itemName}
+                </div>
+                <div style={{ color: '#63687A', marginBottom: '0.5rem' }}>
+                  {thing.description}
+                </div>
+                <input type="button" value="Order Now!" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/data.js
+++ src/data.js
@@ -1,53 +1,220 @@
 const data = [
   {
     id: '1',
-    name: 'Bihar',
+    name: 'Patna',
+    description: 'Hello This is Bihar',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     places: [
-      { area: 'Golghar', description: 'This is golghar' },
-      { area: 'Bihar Museum', description: 'This is Bihar Museum' },
-      { area: 'Patna museum', description: 'This is Patna Musseum' },
-      { area: 'Sanjay Gandhi Biologial Park', description: 'This is Patna Zoo' },
-      { area: 'Takhat Sri Harimandir Ji', description: 'This is Patna Sahib' },
-      { area: 'Mahatama Gandhi Setu', description: 'This is Gandhi Setu' },
+      {
+        area: 'Red Fort',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: "Humayun' Tomb",
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Qutab Minar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'India Gate',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Jama Masjid',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Golghar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
     ],
     items: [
-      { itemName: 'Madhubani Painting', description: 'Hand-made Painting' },
-      { itemName: 'Lacquer Works', description: 'Artistic lacquer ware craft' },
-      { itemName: 'Wood Carvings', description: 'Ancient art of wood carving' },
+      {
+        itemName: 'Madhubani Painting',
+        description: 'Hand-made Painting',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Lacquer Works',
+        description: 'Artistic lacquer ware craft',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Wood Carvings',
+        description: 'Ancient art of wood carving',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
     ],
   },
   {
     id: '2',
     name: 'Delhi',
+    description: 'Hello This is Delhi',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+
     places: [
-      { area: 'Red Fort', description: 'This is golghar' },
-      { area: 'Humayun\' Tomb', description: 'This is golghar' },
-      { area: 'Qutab Minar', description: 'This is golghar' },
-      { area: 'India Gate', description: 'This is golghar' },
-      { area: 'Jama Masjid', description: 'This is golghar' },
-      { area: 'Golghar', description: 'This is golghar' },
+      {
+        area: 'Red Fort',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: "Humayun' Tomb",
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Qutab Minar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'India Gate',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Jama Masjid',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Golghar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
     ],
     items: [
-      { itemName: 'Madhubani Painting', description: 'Very Costly Painting' },
-      { itemName: 'Madhubani Painting', description: 'Very Costly Painting' },
-      { itemName: 'Madhubani Painting', description: 'Very Costly Painting' },
+      {
+        itemName: 'Madhubani Painting',
+        description: 'Hand-made Painting',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Lacquer Works',
+        description: 'Artistic lacquer ware craft',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Wood Carvings',
+        description: 'Ancient art of wood carving',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
     ],
   },
   {
     id: '3',
     name: 'Mumbai',
+    description: 'Hello This is Mumbai',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+
     places: [
-      { area: 'Golghar', description: 'This is golghar' },
-      { area: 'Golghar', description: 'This is golghar' },
-      { area: 'Golghar', description: 'This is golghar' },
-      { area: 'Golghar', description: 'This is golghar' },
-      { area: 'Golghar', description: 'This is golghar' },
-      { area: 'Golghar', description: 'This is golghar' },
+      {
+        area: 'Red Fort',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: "Humayun' Tomb",
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Qutab Minar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'India Gate',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Jama Masjid',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Golghar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
     ],
     items: [
-      { itemName: 'Madhubani Painting', description: 'Very Costly Painting' },
-      { itemName: 'Madhubani Painting', description: 'Very Costly Painting' },
-      { itemName: 'Madhubani Painting', description: 'Very Costly Painting' },
+      {
+        itemName: 'Madhubani Painting',
+        description: 'Hand-made Painting',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Lacquer Works',
+        description: 'Artistic lacquer ware craft',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Wood Carvings',
+        description: 'Ancient art of wood carving',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
     ],
   },
 ];
