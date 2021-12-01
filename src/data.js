9f546965f4e0cbae07a4083cad43f030d66f63c7
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-12-01 22:50:55 +0530

    added some more data

--- src/data.js
+++ src/data.js
@@ -2,52 +2,194 @@ const data = [
   {
     id: '1',
     name: 'Bihar',
+    description: 'Hello This is Bihar',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1622194993926-1801586d460f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
     places: [
-      { area: 'Golghar', description: 'This is golghar' },
-      { area: 'Bihar Museum', description: 'This is Bihar Museum' },
-      { area: 'Patna museum', description: 'This is Patna Musseum' },
-      { area: 'Sanjay Gandhi Biologial Park', description: 'This is Patna Zoo' },
-      { area: 'Takhat Sri Harimandir Ji', description: 'This is Patna Sahib' },
-      { area: 'Mahatama Gandhi Setu', description: 'This is Gandhi Setu' },
+      {
+        area: 'Gol Ghar',
+        description: 'This is Gol Ghar',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/golghar/3300X2400.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        area: "Mahabodhi Temple",
+        description: 'This is Mahabodhi Temple',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/gaya/mahabodhi_temple/buddhist_gaya_category_a_mahabodhi_temple__pic_13.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        area: 'Mahatama Gandhi Setu',
+        description: 'This is Gandhi Setu',
+        subImg:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Gandhi_Setu_Bridge_in_Patna%2C_India.jpg/1024px-Gandhi_Setu_Bridge_in_Patna%2C_India.jpg',
+      },
+      {
+        area: 'Bihar Museum',
+        description: 'This is Bihar Museum',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/bihar_museum/bihar-museum.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        area: 'Takhat Sri Harimandir Ji',
+        description: 'This is Patna Sahib',
+        subImg:
+          'http://www.pmc.bihar.gov.in/assets/img/patna%20sahib.jpg',
+      },
+      {
+        area: 'Vishwa Shanti Stupa',
+        description: 'This is Vishwa Shanti Stupa',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/nalanda/shanti_stupa/vishwa-shanti-stupa-rajgir.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
     ],
     items: [
-      { itemName: 'Madhubani Painting', description: 'Hand-made Painting' },
-      { itemName: 'Lacquer Works', description: 'Artistic lacquer ware craft' },
-      { itemName: 'Wood Carvings', description: 'Ancient art of wood carving' },
+      {
+        itemName: 'Madhubani Painting',
+        description: 'Hand-made Painting',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/madhubani/madhubani_1.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        itemName: 'Stone Craft',
+        description: 'Artistic hand carved craft',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/stone/10.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        itemName: 'Wood Carvings',
+        description: 'Ancient art of wood carving',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/wood/4.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
     ],
   },
   {
     id: '2',
     name: 'Delhi',
+    description: 'Hello This is Delhi',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+
     places: [
-      { area: 'Red Fort', description: 'This is Red Fort' },
-      { area: 'Humayun\' Tomb', description: 'This is Humayun\' Tomb' },
-      { area: 'Qutab Minar', description: 'This is Qutab Minar' },
-      { area: 'India Gate', description: 'This is India Gate' },
-      { area: 'Jama Masjid', description: 'This is Jama Masjid' },
-      { area: 'Lotus Temple', description: 'This is Lotus Temple' },
+      { area: 'Red Fort',
+      description: 'This is golghar',
+      subImg:
+        'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      area: "Humayun' Tomb",
+      description: 'This is golghar',
+      subImg:
+        'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      area: 'Qutab Minar',
+      description: 'This is golghar',
+      subImg:
+        'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
     ],
     items: [
-      { itemName: 'Khadi', description: 'Very Costly Painting' },
-      { itemName: 'Musical Instruments', description: 'Very Costly Painting' },
-      { itemName: 'Pashmina Shawl', description: 'Very Costly Painting' },
+      {
+        itemName: 'Madhubani Painting',
+        description: 'Hand-made Painting',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Lacquer Works',
+        description: 'Artistic lacquer ware craft',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
     ],
   },
   {
     id: '3',
     name: 'Mumbai',
+    description: 'Hello This is Mumbai',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+
     places: [
-      { area: 'Chhatrapati Shivaji Maharaj Terminus', description: 'Chhatrapati Shivaji Maharaj Terminus' },
-      { area: 'Gateway of India', description: 'Gateway of India' },
-      { area: 'Elephanta Caves', description: 'Elephanta Caves' },
-      { area: 'Marine Drive', description: 'Marine Drive' },
-      { area: 'Shri Siddhi Vinayak Ganpati Mandir', description: 'Shri Siddhi Vinayak Ganpati Mandir' },
-      { area: 'Kanheri Caves', description: 'Kanheri Caves' },
+      {
+        area: 'Red Fort',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: "Humayun' Tomb",
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Qutab Minar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'India Gate',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Jama Masjid',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
+      {
+        area: 'Golghar',
+        description: 'This is golghar',
+        subImg:
+          'https://images.unsplash.com/photo-1524230507669-5ff97982bb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      },
     ],
     items: [
-      { itemName: 'Leather Goods', description: 'Very Costly Painting' },
-      { itemName: 'Incense and Perfume', description: 'Very Costly Painting' },
-      { itemName: 'Jewelry', description: 'Very Costly Painting' },
+      {
+        itemName: 'Madhubani Painting',
+        description: 'Hand-made Painting',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Lacquer Works',
+        description: 'Artistic lacquer ware craft',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
+      {
+        itemName: 'Wood Carvings',
+        description: 'Ancient art of wood carving',
+        itemImg:
+          'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      },
     ],
   },
 ];
